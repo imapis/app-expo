c9147d854ce02d97aa9bdd1ba8ff3c3dfb8c8fda
imapis <im4p1s@gmail.com>
2024-06-04 19:01:44 +0200

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+      <Text> PUC Minas - by Mapis </Text>
+    </View>
+  );
+}
+
+export default HomePage
